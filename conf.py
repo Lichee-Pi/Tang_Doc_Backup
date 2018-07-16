7513a6bea1d9979c1974214fb817a2aa9419d45f
Big7lion <biglion77@outlook.com>
2018-07-16 14:10:09 +0800

    .

--- conf.py
+++ conf.py
@@ -70,7 +70,6 @@ exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store','.vscode']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
